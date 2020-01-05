11c8b6b2ce10271c31792054503ac213789e7374
Cristina Pena <cristinalpena@gmail.com>
2020-01-05 11:32:15 -0700

    Pass author tests

--- author.js
+++ author.js
@@ -1,5 +1,19 @@
-class Author {
+var Book = require('./book.js');
 
+class Author {
+  constructor(authorObj){
+    this.first = authorObj.first;
+    this.last = authorObj.last;
+    this.books = [];
+  }
+  write(title, publicationYear){
+    var newBook = new Book(this.first, this.last, title, publicationYear);
+    this.books.push(newBook);
+    return newBook;
+  }
 }
 
 module.exports = Author;
+
+
+// npm test author-test.js
