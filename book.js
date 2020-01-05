cba9a21b86170819a52ebbeb9469984dfedd3abf
Cristina Pena <cristinalpena@gmail.com>
2020-01-05 10:42:54 -0700

    Pass book tests

--- book.js
+++ book.js
@@ -1,5 +1,13 @@
 class Book {
-
+  constructor(firstName, lastName, title, publicationYear){
+    this.author = `${firstName} ${lastName}`;
+    this.title = title;
+    this.publicationYear = publicationYear;
+  }
 }
 
 module.exports = Book;
+
+
+// npm test book-test.js
+// npm test author-test.js
